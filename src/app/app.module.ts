0bb7ca889990b33fb19d1ad3d1b34bf82a46a75a
DvirMon <dmenajem@gmail.com>
2022-07-27 17:31:54 +0300

    feat (app-module) -  remove unuse imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ErrorsService } from './services/error.service';
 import { HttpErrorInterceptor } from './utilities/error.interceptor';
-import { LobbyComponent } from './components/lobby/lobby.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+
+import { LobbyComponent } from './components/lobby/lobby.component';
 
 @NgModule({
   declarations: [
