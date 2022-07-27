4ad3c5717d7c352573d6ce9a183439e7fcf21ba1
DvirMon <dmenajem@gmail.com>
2022-07-27 17:44:51 +0300

    feat (favorites.module) - add favorites component

--- src/app/favorites/favorites.module.ts
+++ src/app/favorites/favorites.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FavoritesRoutingModule } from './favorites-routing.module';
+import { FavoritesLayoutComponent } from './components/favorites-layout/favorites-layout.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FavoritesLayoutComponent
+  ],
   imports: [
     CommonModule,
-    FavoritesRoutingModule
+    FavoritesRoutingModule,
+    SharedModule
   ]
 })
 export class FavoritesModule { }
