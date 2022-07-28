9db07112b0ef6580c24855fb9e5ad4a6920c0ae6
DvirMon <dmenajem@gmail.com>
2022-07-28 10:43:04 +0300

    feat (lobby) - weather result ui works

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterielModule } from './materiel/materiel.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { WeatherResultComponent } from './components/weather-result/weather-result.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WeatherResultComponent
+  ],
   imports: [
     CommonModule,
     MaterielModule,
@@ -16,6 +19,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MaterielModule,
     FlexLayoutModule,
     ReactiveFormsModule,
+
+    WeatherResultComponent
   ],
 })
 export class SharedModule {}
