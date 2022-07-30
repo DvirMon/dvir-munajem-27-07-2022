795833e8d7628c7edd68cec90db12121599ad3b2
DvirMon <dmenajem@gmail.com>
2022-07-30 21:10:39 +0300

    feat (favorite) - aff touster postion class

--- src/app/favorites/guards/favorite.guard.ts
+++ src/app/favorites/guards/favorite.guard.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, tap } from 'rxjs';
 import { AppSelectors } from 'src/app/ngrx/app.types';
 
 @Injectable({
@@ -10,13 +11,22 @@ import { AppSelectors } from 'src/app/ngrx/app.types';
 export class FavoriteGuard implements CanActivate {
 
   constructor(
-    private store: Store<any>
+    private router: Router,
+    private store: Store<any>,
+    private toastr : ToastrService
   ) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.store.select(AppSelectors.hasFavorites);
+    return this.store.select(AppSelectors.hasFavorites).pipe(
+      tap((hasFavorites: boolean) =>{
+
+        if(!hasFavorites) {
+          this.toastr.show('No Favorites has been selected')
+          this.router.navigateByUrl('/')
+        }
+      }));
   }
 
 }
