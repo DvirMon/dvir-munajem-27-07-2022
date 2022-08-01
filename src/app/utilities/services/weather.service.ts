2c836847fa74fdfee2622cb2eedab422f03c8178
DvirMon <dmenajem@gmail.com>
2022-08-01 12:58:48 +0300

    feat (weather.service) - remove mock_data

--- src/app/utilities/services/weather.service.ts
+++ src/app/utilities/services/weather.service.ts
@@ -2,8 +2,6 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
-import { CURRENT_WEATHER, FUTURE_WEATHER, GEOLOCATION_DATA, LOCATIONS_AUTOCOMPLETE } from '../mock_data/data';
-
 import { AutocompleteResult } from '../models/autocomplete-result';
 import { CurrentWeatherResult } from '../models/current-weather-result';
 import { AutocompleteOption } from '../models/autocomplete-option';
@@ -33,16 +31,12 @@ export class WeatherService {
 
   private _getLocationAutocomplete(query: string): Observable<AutocompleteResult[]> {
 
-    const data = LOCATIONS_AUTOCOMPLETE.filter((item: AutocompleteResult) => item.LocalizedName.toLowerCase().includes(query.toLowerCase()));
-    const action = AppActions.SetSearchResult({ data });
-    this.store.dispatch(action);
-    return of(data);
-    // const params = new HttpParams().set('apikey', environment.accuWeatherAPIKey).set('q', query)
-    // return this.http.get<AutocompleteResult[]>(this._baseUrl + 'locations/v1/cities/autocomplete', { params }).pipe(
-    // tap((data: AutocompleteResult[]) => {
-    //   const action = AppActions.SetSearchResult({ data })
-    //   this.store.dispatch(action)
-    // }))
+    const params = new HttpParams().set('apikey', environment.accuWeatherAPIKey).set('q', query)
+    return this.http.get<AutocompleteResult[]>(this._baseUrl + 'locations/v1/cities/autocomplete', { params }).pipe(
+      tap((data: AutocompleteResult[]) => {
+        const action = AppActions.SetSearchResult({ data })
+        this.store.dispatch(action)
+      }))
   }
 
   getLocationOptions(query: string): Observable<AutocompleteOption[]> {
@@ -60,9 +54,8 @@ export class WeatherService {
 
       filter((data) => !data.hasOwnProperty(locationKey)),
       switchMap(() => {
-        // const params = new HttpParams().set('apikey', environment.accuWeatherAPIKey)
-        // return this.http.get<CurrentWeatherResult[]>(this._baseUrl + 'currentconditions/v1/' + locationKey, { params })
-        return of(CURRENT_WEATHER)
+        const params = new HttpParams().set('apikey', environment.accuWeatherAPIKey)
+        return this.http.get<CurrentWeatherResult[]>(this._baseUrl + 'currentconditions/v1/' + locationKey, { params })
           .pipe(
             map((data: CurrentWeatherResult[]) => {
               const action = AppActions.SetCurrentWeather({ data: data[0], id: locationKey })
@@ -93,9 +86,8 @@ export class WeatherService {
     ), metric$])
       .pipe(switchMap(([data, metric]) => {
 
-        // const params = new HttpParams().set('apikey', environment.accuWeatherAPIKey).append('metric', metric)
-        // return this.http.get<FutureResultObject>(this._baseUrl + 'forecasts/v1/daily/5day/' + locationKey, { params })
-        return of(FUTURE_WEATHER)
+        const params = new HttpParams().set('apikey', environment.accuWeatherAPIKey).append('metric', metric)
+        return this.http.get<FutureResultObject>(this._baseUrl + 'forecasts/v1/daily/5day/' + locationKey, { params })
           .pipe(
             tap((data: FutureResultObject) => {
               const action = AppActions.SetFutureWeather({ data, id: locationKey })
